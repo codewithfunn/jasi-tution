f8af3236de8e85aeac9e6a4524583478cfbaf7ed
jaspreet_suri <160199423+SKTECHZ@users.noreply.github.com>
2024-02-25 00:47:03 +0530

    clerk auth setup.

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex h-full item-center justify-center">{children}</div>
+  );
+};
+
+export default AuthLayout;
