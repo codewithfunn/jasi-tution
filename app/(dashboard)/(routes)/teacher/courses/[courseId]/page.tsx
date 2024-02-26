fea8cd2a7603a387a9d47de2eac15992cfe8d14d
jaspreet_suri <160199423+SKTECHZ@users.noreply.github.com>
2024-02-26 17:40:56 +0530

    created the course attachment component and also created the required backend for this

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,6 +1,6 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { IndianRupee, LayoutDashboard, ListChecks } from "lucide-react";
+import { File, IndianRupee, LayoutDashboard, ListChecks } from "lucide-react";
 
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
@@ -10,12 +10,9 @@ import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
 
-const CourseIdPage = async ({
-  params
-}: {
-  params: { courseId: string }
-}) => {
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
 
   if (!userId) {
@@ -24,7 +21,14 @@ const CourseIdPage = async ({
 
   const course = await db.course.findUnique({
     where: {
-      id: params.courseId
+      id: params.courseId,
+    },
+    include: {
+      attachments:{
+        orderBy:{
+          createdAt:"desc"
+        }
+      }
     }
   });
 
@@ -43,21 +47,19 @@ const CourseIdPage = async ({
     course.description,
     course.imageUrl,
     course.price,
-    course.categoryId
+    course.categoryId,
   ];
 
   const totalFields = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
 
-  const completionText = `(${completedFields}/${totalFields})`
+  const completionText = `(${completedFields}/${totalFields})`;
 
-  return ( 
+  return (
     <div className="p-6">
       <div className="flex items-center justify-between">
         <div className="flex flex-col gap-y-2">
-          <h1 className="text-2xl font-medium">
-            Course setup
-          </h1>
+          <h1 className="text-2xl font-medium">Course setup</h1>
           <span className="text-sm text-slate-700">
             Complete all fields {completionText}
           </span>
@@ -67,22 +69,11 @@ const CourseIdPage = async ({
         <div>
           <div className="flex items-center gap-x-2">
             <IconBadge icon={LayoutDashboard} />
-            <h2 className="text-xl">
-              Customize your course
-            </h2>
+            <h2 className="text-xl">Customize your course</h2>
           </div>
-          <TitleForm
-            initialData={course}
-            courseId={course.id}
-          />
-          <DescriptionForm
-            initialData={course}
-            courseId={course.id}
-          />
-          <ImageForm
-            initialData={course}
-            courseId={course.id}
-          />
+          <TitleForm initialData={course} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
+          <ImageForm initialData={course} courseId={course.id} />
           <CategoryForm
             initialData={course}
             courseId={course.id}
@@ -96,30 +87,28 @@ const CourseIdPage = async ({
           <div>
             <div className="flex items-center gap-x-2">
               <IconBadge icon={ListChecks} />
-              <h2 className="text-xl">
-                Course chapters
-              </h2>
+              <h2 className="text-xl">Course chapters</h2>
             </div>
-            <div>
-              TODO: Chapters
-            </div>
+            <div>TODO: Chapters</div>
           </div>
           <div>
             <div className="flex items-center gap-x-2">
               <IconBadge icon={IndianRupee} />
-              <h2 className="text-xl">
-                Sell your course
-              </h2>
+              <h2 className="text-xl">Sell your course</h2>
             </div>
-            <PriceForm
-              initialData={course}
-              courseId={course.id}
-            />
+            <PriceForm initialData={course} courseId={course.id} />
+          </div>
+          <div className="">
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={File} />
+              <h2 className="text-xl">Recourses & Attachment</h2>
+            </div>
+            <AttachmentForm initialData={course} courseId={course.id} />
           </div>
         </div>
       </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default CourseIdPage;
