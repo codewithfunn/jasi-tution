84d489fa1abc660ae119a5974d29b37907b885f7
jaspreet_suri <160199423+SKTECHZ@users.noreply.github.com>
2024-02-26 13:37:11 +0530

    setup the comboBox and create the category form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -8,6 +8,7 @@ import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({
   params
@@ -15,7 +16,7 @@ const CourseIdPage = async ({
   params: { courseId: string }
 }) => {
   const { userId } = auth();
-    console.log(userId)
+
   if (!userId) {
     return redirect("/");
   }
@@ -24,6 +25,12 @@ const CourseIdPage = async ({
     where: {
       id: params.courseId
     }
+  });
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
   });
 
   if (!course) {
@@ -42,6 +49,7 @@ const CourseIdPage = async ({
   const completedFields = requiredFields.filter(Boolean).length;
 
   const completionText = `(${completedFields}/${totalFields})`
+
   return ( 
     <div className="p-6">
       <div className="flex items-center justify-between">
@@ -73,6 +81,14 @@ const CourseIdPage = async ({
           <ImageForm
             initialData={course}
             courseId={course.id}
+          />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
           />
         </div>
       </div>
