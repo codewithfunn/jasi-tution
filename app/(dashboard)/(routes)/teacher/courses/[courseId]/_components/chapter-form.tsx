f22fcacf91bfe65af031bb826eabccab46a57561
jaspreet_suri <160199423+SKTECHZ@users.noreply.github.com>
2024-02-26 22:42:21 +0530

    setup the reorder Functionality to our course Chapter using  @hello-pangea/dnd

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-form.tsx
@@ -4,7 +4,7 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
@@ -21,9 +21,10 @@ import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 import { Chapter, Course } from "@prisma/client";
+import { ChaptersList } from "./chapter-list";
 
 interface ChapterFormProps {
-  initialData: Course & {chapters: Chapter[]};
+  initialData: Course & { chapters: Chapter[] };
   courseId: string;
 }
 
@@ -31,10 +32,7 @@ const formSchema = z.object({
   title: z.string().min(1),
 });
 
-export const ChapterForm = ({
-  initialData,
-  courseId,
-}: ChapterFormProps) => {
+export const ChapterForm = ({ initialData, courseId }: ChapterFormProps) => {
   const [isCreating, setIsCreating] = useState(false);
   const [isUpdating, setIsUpdating] = useState(false);
 
@@ -44,9 +42,9 @@ export const ChapterForm = ({
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: { 
-      title: ""
-     }, 
+    defaultValues: {
+      title: "",
+    },
   });
 
   const { isSubmitting, isValid } = form.formState;
@@ -61,9 +59,32 @@ export const ChapterForm = ({
       toast.error("Something went wrong");
     }
   };
-
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData,
+      });
+      toast.success("Chapters reordered");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+  const onEdit = async (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+  }
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+      {
+        isUpdating && (
+          <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
+            <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+          </div>
+        )
+      }
       <div className="font-medium flex items-center justify-between">
         Course Chapters
         <Button onClick={toggleCreating} variant="ghost">
@@ -99,29 +120,32 @@ export const ChapterForm = ({
                 </FormItem>
               )}
             />
-              <Button disabled={!isValid || isSubmitting} type="submit">
-                Create
-              </Button>
+            <Button disabled={!isValid || isSubmitting} type="submit">
+              Create
+            </Button>
           </form>
         </Form>
       )}
-      {
-        !isCreating && (
-          <div className={cn(
-            "mt-2 text-sm ", !initialData.chapters.length && "text-slate-500 italic",
-          )}>
-            {!initialData.chapters.length && "No chapter"}
-            {/* TODO: Add a list of chapter */}
-          </div>
-        )
-      }
-      {
-        !isCreating && (
-          <p className="text-xs text-muted-foreground mt-4">
-            Drag and drop to reorder the chapters
-          </p>  
-        )
-      }
+      {!isCreating && (
+        <div
+          className={cn(
+            "mt-2 text-sm ",
+            !initialData.chapters.length && "text-slate-500 italic"
+          )}
+        >
+          {!initialData.chapters.length && "No chapter"}
+          <ChaptersList
+            onEdit={onEdit}
+            onReorder={onReorder}
+            items={initialData.chapters || []}
+          />
+        </div>
+      )}
+      {!isCreating && (
+        <p className="text-xs text-muted-foreground mt-4">
+          Drag and drop to reorder the chapters
+        </p>
+      )}
     </div>
   );
 };
